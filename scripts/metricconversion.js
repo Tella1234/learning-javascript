a2df18d3f86f0e2718ce64dcd10be9a6c6aea3cf
Tella Boluwatife <boluwatifetella@gmail.com>
2022-04-22 11:10:56 +0100

    Metric Conversion Done

--- scripts/metricconversion.js
+++ scripts/metricconversion.js
@@ -3,11 +3,27 @@ let meterInputs = document.getElementById('meters-input')
 let feetOutputs = document.getElementById('feet-output')
 let feetInputs = document.getElementById('feet-input')
 let meterOutputs = document.getElementById('meters-output')
+let litersInputs = document.getElementById('liters-input')
+let gallonsOutputs = document.getElementById('gallons-output')
+let gallonsInputs = document.getElementById('gallons-input')
+let litersOutputs = document.getElementById('liters-output')
+let kgInputs = document.getElementById('kg-input')
+let poundsOutputs = document.getElementById('pounds-output')
+let poundsInputs = document.getElementById('pounds-input')
+let kgOutputs = document.getElementById('kg-output')
 
 inputNumber.addEventListener('input', updateInput);
 inputNumber.addEventListener('input', feetOutput);
 inputNumber.addEventListener('input', feetInput);
 inputNumber.addEventListener('input', meterOutput);
+inputNumber.addEventListener('input', literInput);
+inputNumber.addEventListener('input', galonOutput);
+inputNumber.addEventListener('input', galonInput);
+inputNumber.addEventListener('input', literOutput);
+inputNumber.addEventListener('input', kgInput);
+inputNumber.addEventListener('input', poundsOutput);
+inputNumber.addEventListener('input', poundsInput);
+inputNumber.addEventListener('input', kgOutput);
 
 
 function updateInput(e) {
@@ -15,7 +31,7 @@ function updateInput(e) {
 }
 
 function feetOutput(e) {
-    feetOutputs.textContent = (e.target.value * 3.2808).toFixed(4);
+    feetOutputs.textContent = (e.target.value * 3.2808).toFixed(2);
 }
 
 function feetInput(e) {
@@ -23,5 +39,36 @@ function feetInput(e) {
 }
 
 function meterOutput(e) {
-    meterOutputs.textContent = (e.target.value / 3.2808).toFixed(4);
+    meterOutputs.textContent = (e.target.value / 3.2808).toFixed(2);
+}
+
+function literInput(e) {
+    litersInputs.textContent = e.target.value;
+}
+
+function galonOutput(e) {
+    gallonsOutputs.textContent = (e.target.value / 3.7854).toFixed(3);
+}
+
+function galonInput(e) {
+    gallonsInputs.textContent = e.target.value;
+}
+
+function literOutput(e) {
+    litersOutputs.textContent = (e.target.value * 3.7854).toFixed(3);
+}
+function kgInput(e) {
+    kgInputs.textContent = e.target.value;
+}
+
+function poundsOutput(e) {
+    poundsOutputs.textContent = (e.target.value * 2.20462).toFixed(3);
+}
+
+function poundsInput(e) {
+    poundsInputs.textContent = e.target.value;
+}
+
+function kgOutput(e) {
+    kgOutputs.textContent = (e.target.value / 2.20462).toFixed(3);
 }
