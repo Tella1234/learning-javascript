1159aef3f04188e50731a29fc27bc2367d113831
IncomingAnonymous <boluwatifetella@gmail.com>
2022-05-23 12:44:56 +0100

    Blackjack completed

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,23 +1,66 @@
-let firstCard = 3;
-let secondCard = 9;
-let sum = firstCard + secondCard;
-
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack")
-} else {
-    console.log("You're out of the game")
+let player = {
+    name: "Pallas",
+    chips: 200
 }
 
-let hasSolvedChallenge = false
-let hasHintsLeft = false
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
-// Create an if statement that checks that both variables are false.
-// If so, run the showSolution() function
-if( hasSolvedChallenge === false && hasHintsLeft === false) {
-    showSolution();
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
-function showSolution() {
-    console.log("Showing the solution....")
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+    
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./pages/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'nunito': ['nunito', 'sans-serif'],
+    },
   },
   plugins: [],
 }
