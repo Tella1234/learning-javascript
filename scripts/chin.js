79d28653b75c21ae46c15909f6b9c2257fa4be59
IncomingAnonymous <boluwatifetella@gmail.com>
2022-05-30 08:58:52 +0100

    Chin Chin Inventory

--- scripts/chin.js
+++ scripts/chin.js
@@ -1,5 +1,6 @@
 
-let inputNumber = document.querySelector('input')
+let take = document.getElementById("take")
+let inputNumber = document.querySelector("input");
 let cupBtn = document.getElementById("cup")
 let piecesBtn = document.getElementById("pieces")
 let refillBtn = document.getElementById("refill")
@@ -9,32 +10,41 @@ chin.innerText = "Chin Chin: " + chinKg + "Kg";
 
 cupBtn.addEventListener("click", function() {
     if (chinKg < 3) {
-        chinKg = "You are low on Chin Chin !!! Remaining less than 3";
+        alert("You are low on Chin Chin !!! Remaining less than 3Kg");
     } else {
         chinKg -= 3;
     }
     chin.innerText = "Chin Chin: " + chinKg + "Kg";
 })
+
 piecesBtn.addEventListener("click", function() {
     if (chinKg == 0) {
-        chinKg.innerText = "You are out of Chin Chin";
+        alert("You are out of Chin Chin");
     } else {
         chinKg -= 1;
     }
     
     chin.innerText = "Chin Chin: " + chinKg + "Kg";
 })
+
 refillBtn.addEventListener("click", function() {
-    if (chinKg <=2) {
+    if (chinKg <= 20) {
         chinKg = 100;
+    } else {
+        alert("You still have enough Chin Chin")
     }
     chin.innerText = "Chin Chin: " + chinKg + "Kg";
 })
-inputNumber.addEventListener('input', function(e) {
-    if (chinKg < e.target.value) {
-        chinKg = "You don't have enough Chin Chin !!!";
-    } else {
-        chinKg -= e.target.value;
+
+inputNumber.addEventListener("input", function(e) {
+    inputValue = e.target.value
+})
+
+take.addEventListener('click', function() {
+    if (chinKg < inputValue) {
+        alert("You don't have enough Chin Chin !!!");
+    } else if (chinKg > inputValue) {
+        chinKg -= inputValue;
     }
     chin.innerText = "Chin Chin: " + chinKg + "Kg";
 })
