efa92e37f60d4c8a1b2c719ca0809a9d75fd0698
IncomingAnonymous <boluwatifetella@gmail.com>
2022-06-19 20:37:09 +0100

    Reviews finished

--- scripts/review.js
+++ scripts/review.js
@@ -11,21 +11,28 @@ const reviews = [
     id: 2,
     name: "anna johnson",
     job: "web designer",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    img: "/assets/images/person3.jpg",
     text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
     id: 3,
     name: "peter jones",
     job: "intern",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    img: "/assets/images/person4.jpg",
     text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
     id: 4,
+    name: "sara jones",
+    job: "UX Designer",
+    img: "/assets/images/person-1.jpeg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero?    Iste, doloremque nihil?",
+  },
+  {
+    id: 5,
     name: "bill anderson",
     job: "the boss",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    img: "/assets/images/person5.jpg",
     text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
@@ -73,4 +80,10 @@ prevBtn.addEventListener('click', function() {
     currentItem = reviews.length - 1;
   }
   showPerson(currentItem);
+})
+
+// Show random reviews
+randomBtn.addEventListener('click', function() {
+  currentItem = (Math.floor(Math.random() * reviews.length));
+  showPerson(currentItem)
 })
