4d9568fbe97346900941645868bd637d8a10bcbe
IncomingAnonymous <boluwatifetella@gmail.com>
2022-07-02 18:43:56 +0100

    Password Generator Completed

--- scripts/passwordgenerator.js
+++ scripts/passwordgenerator.js
@@ -26,14 +26,44 @@ function genPassword() {
     
 }    
 
-    function clearPassword() {
-        const dotsEls = document.querySelectorAll(".dots");
-        for (let el of dotsEls) {
-            el.style.display = "block";
-        }
-    
-        const passwordEls = document.querySelectorAll(".password")
-        for (let el of passwordEls) {
-            el.innerHTML = "";
-        }
+function clearPassword() {
+    const dotsEls = document.querySelectorAll(".dots");
+    for (let el of dotsEls) {
+        el.style.display = "block";
     }
+
+    const passwordEls = document.querySelectorAll(".password")
+    for (let el of passwordEls) {
+        el.innerHTML = "";
+    }
+}
+
+const passwordContainer = document.querySelectorAll(".password-container")
+
+passwordContainer.forEach(
+    function(password) {
+        password.addEventListener('click', function() {
+            const copyText = password.querySelector(".password")
+
+            if (copyText.textContent == "") {
+                const error = document.getElementById("error");
+
+                error.style.display = "block"
+                
+                setTimeout(function() {
+                    error.style.display = "none"
+                }, 2000)
+            } else {
+                navigator.clipboard.writeText(copyText.textContent)
+    
+                const copied = document.getElementById("copied");
+    
+                copied.style.display = "block"
+                
+                setTimeout(function() {
+                    copied.style.display = "none"
+                }, 2000)
+            }  
+        })
+    }
+)
